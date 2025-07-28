74cc2697606fa2953eddd35b80405cf346361bef
CorentinBARUT <126794551+CorentinBARUT@users.noreply.github.com>
2025-07-28 12:39:57 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-<<<<<<< HEAD
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
@@ -9,14 +8,4 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
   </React.StrictMode>,
 );
-=======
-     import ReactDOM from 'react-dom/client';
-     import App from './App.jsx';
-     import './index.css';
 
-     ReactDOM.createRoot(document.getElementById('root')).render(
-       <React.StrictMode>
-         <App />
-       </React.StrictMode>,
-     );
->>>>>>> b9b8e22dbe1e5e36be042a646a3139c891d22a25
